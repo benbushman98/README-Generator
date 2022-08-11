595d47d31b3a4d7e4693786a4d928ef0650adc5e
Benjamin Bushman <benbushman98@gmail.com>
2022-08-10 19:25:50 -0600

    updated index.js with questions and inquirer. Created template in generate.

---
+++ index.js
@@ -0,0 +1,65 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const md = require('./utils/generateMarkdown')
+
+
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Please write a description of your project:',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Please describe specific installation instructions.',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Please describe specific usage information.',
+        name: 'usage',
+    },
+    {
+        type: 'checkbox',
+        message: 'Please choose a license.',
+        name: 'license',
+        choices: [
+            'MIT',
+            'etc',
+        ]
+    },
+    {
+        type: 'input',
+        message: 'Please describe specific contribution guidlines.',
+        name: 'contribution',
+    },
+    {
+        type: 'input',
+        message: 'Please describe specific testing instructions.',
+        name: 'test',
+    },
+    {
+        type: 'input',
+        message: 'Please enter your GitHub username.',
+        name: 'github_username',
+    },
+    {
+        type: 'input',
+        message: 'Please enter the email address you want to display for contact information.',
+        name: 'email',
+    },
+]
+
+
+
+inquirer
+    .prompt(questions)
+    .then((data) => {
+        fs.writeFile('README.md', md(data), (err) =>
+            err ? console.error(err) : console.log('Success!'))
+    })

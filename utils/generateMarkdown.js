739db9ef4a11d2f86762f55a1cdf4c65f3de170f
Benjamin Bushman <benbushman98@gmail.com>
2022-08-12 18:36:49 -0600

    added info for rendering license info and also for badges info

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,29 +1,47 @@
+function renderLicenseBadge(license) {
+  switch (license) {
+    case'MIT':
+      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+    case 'GPLv2':
+      return `[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`
+    case 'Apache 2.0':
+      return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+    case 'Unlicense':
+      return `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`
+    case 'None':
+      return ""
+  }
+}
+
 function renderLicenseSection(license) {
   switch (license) {
-    case 'MIT':
-      return `MIT \n This project is licensed under the `
-      break;
+    case'MIT':
+      const heremit = "https://opensource.org/licenses/MIT";
+      return `This project is licensed under the MIT license.\nPlease visit to get more information: ${heremit}`
     case 'GPLv2':
-      console.log('GPLv2!');
-      break;
+      const heregpl = "https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html";
+      return `This project is licensed under the GPLv2 license.\nPlease visit to get more information: ${heregpl}`
     case 'Apache 2.0':
-      console.log('Apache 2.0');
-      break;
-    case 'The Unlicense':
-      console.log('Unlicense!');
-      break;
-    default:
-      console.log('Default!');
+      const hereapache = "https://www.apache.org/licenses/LICENSE-2.0";
+      return `This project is licensed under the Apache 2.0 license.\nPlease visit to get more information: ${hereapache}`
+    case 'Unlicense':
+      const hereunlicense = "https://unlicense.org/";
+      return `This project uses The Unlicense template.\nPlease visit to get more information: ${hereunlicense}`
+    case 'None':
+      return ""
   }
-  
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+${renderLicenseBadge(data.license)}
+
 ## Description
 ${data.description}
+
+---
 
 ## Table of Contents
 - [Installation](#installation)
@@ -33,20 +51,32 @@ ${data.description}
 - [Tests](#test)
 - [Questions](#questions)
 
+---
+
 ## Installation
 ${data.installation}
+
+---
 
 ## Usage
 ${data.usage}
 
+---
+
 ## License
 ${renderLicenseSection(data.license)}
+
+---
 
 ## Contributing
 ${data.contribution}
 
+---
+
 ## Tests
 ${data.test}
+
+---
 
 ## Questions
 Please visit me at https://github.com/${data.github_username}. 
